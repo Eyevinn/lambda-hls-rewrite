6aad6cd512191fdc61998b6a62a80b3f74ad1e91
Jonas Birmé <jonas.birme@eyevinn.se>
2022-06-20 11:55:23 +0200

    chore: use our dev-lambda lib instead (#1)

--- dev.server.js
+++ dev.server.js
@@ -1,33 +1,3 @@
-const fastify = require("fastify");
+const { LambdaELB } = require("@eyevinn/dev-lambda");
 const { handler } = require("./dist/index.js");
-
-const server = fastify();
-server.get("*", async (request, reply) => {
-  //console.log(request.url);
-  let params = {};
-  const [ path, queryString ] = request.url.split("?");
-  if(queryString) {
-    for (let pair of queryString.split("&")) {
-      const [k, v] = pair.split("=");
-      params[k] = v;
-    }
-  }
-  const event = {
-    requestContext: {
-      elb: {}
-    },
-    path: path,
-    httpMethod: request.method,
-    headers: request.headers,
-    queryStringParameters: params,
-  };
-  const response = await handler(event);
-  reply
-  .code(response.statusCode)
-  .headers(response.headers)
-  .send(response.body);
-});
-
-server.listen(process.env.PORT || 8000, "0.0.0.0", (err, address) => {
-  console.log(`Server listening at ${address}`);
-});
+(new LambdaELB({ handler })).run();
